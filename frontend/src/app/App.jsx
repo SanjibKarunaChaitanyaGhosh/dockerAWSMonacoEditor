65a227f525e83a846b631f1b7695a5e9ee3339e7
SanjibKarunaChaitanyaGhosh <sanjibghosh791@gmail.com>
2026-06-16 10:56:25 +0530

    frontend and backend seperation

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -8,7 +8,7 @@ function App() {
           className="h-screen w-full bg-gray-950  flex gap-4 p-4">
 
             <aside className="h-full w-1/4 bg-amber-50 rounded-lg" ></aside>
-            <section className="w-3/4 bg-neutral-800 rounded-lg">
+            <section className="w-3/4 bg-neutral-800 rounded-lg ">
                 <Editor 
                     height="100%"
                     defaultLanguage="javascript"
